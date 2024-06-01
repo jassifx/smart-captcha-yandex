6879ae6f2bb1b84c28325072294f2a8f82b5a86b
Jaspreet Singh <156990848+jassifx@users.noreply.github.com>
2024-06-01 16:15:14 +0530

    Update class.plugin.php

--- includes/class.plugin.php
+++ includes/class.plugin.php
@@ -12,7 +12,7 @@ class Plugin extends Plugin_Base {
 	 *
 	 * @var Page_Settings
 	 */
-	public $settings;
+	public Page_Settings $settings;
 
 	/**
 	 * Plugin constructor.
@@ -20,7 +20,7 @@ class Plugin extends Plugin_Base {
 	public function __construct() {
 		parent::__construct();
 
-		if ( ! is_admin() ) { // without this check it is not possible to add comment in admin section
+		if ( ! is_admin() ) { // Without this check, it is not possible to add comments in the admin section.
 			add_action( 'wp_head', function () {
 				$comments = new CommentsFormCaptcha();
 			} );
@@ -32,7 +32,7 @@ class Plugin extends Plugin_Base {
 	}
 
 	public function front_enqueue_assets() {
-		//wp_enqueue_script( WYSC_PLUGIN_PREFIX . '_js', WYSC_PLUGIN_URL . '/assets/script.js', [ 'jquery' ], WYSC_PLUGIN_VERSION, true );
+		// wp_enqueue_script( WYSC_PLUGIN_PREFIX . '_js', WYSC_PLUGIN_URL . '/assets/script.js', [ 'jquery' ], WYSC_PLUGIN_VERSION, true );
 		wp_register_script( 'wysc_script', 'https://captcha-api.yandex.ru/captcha.js', [], WYSC_PLUGIN_VERSION, true );
 	}
 
@@ -46,8 +46,6 @@ class Plugin extends Plugin_Base {
 	 */
 	public function admin_code() {
 		$this->register_page( 'Page_Settings' );
-		//$this->register_page( 'Page_Main' );
+		// $this->register_page( 'Page_Main' );
 	}
-
-
 }
