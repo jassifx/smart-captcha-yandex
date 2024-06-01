d482b09e386b2e3e5a738122c55d743db9307779
Jaspreet Singh <156990848+jassifx@users.noreply.github.com>
2024-06-01 16:07:00 +0530

    Update class.page-settings-base.php

--- admin/includes/base/class.page-settings-base.php
+++ admin/includes/base/class.page-settings-base.php
@@ -11,14 +11,14 @@ abstract class PageSettingsBase extends PageBase {
 	/**
 	 * @var array
 	 */
-	protected $settings;
+	protected array $settings;
 
 	/**
 	 * PageSettingsBase constructor.
 	 *
 	 * @param Plugin $plugin
 	 */
-	public function __construct( $plugin ) {
+	public function __construct( Plugin $plugin ) {
 		parent::__construct( $plugin );
 	}
 
@@ -30,14 +30,14 @@ abstract class PageSettingsBase extends PageBase {
 				foreach ( $section['options'] as $opt_name => $option ) {
 					$opt_name          = WYSC_PLUGIN_PREFIX . '_' . $opt_name;
 					$opt_type          = $option['type'] ?? 'text';
-					$render_function   = $option['render_callback'] ?? [ $this, "fill_{$opt_type}_field" ];
-					$sanitize_function = $option['sanitize_callback'] ?? [ $this, 'sanitize_callback' ];
+					$render_function   = $option['render_callback'] ?? array( $this, "fill_{$opt_type}_field" );
+					$sanitize_function = $option['sanitize_callback'] ?? array( $this, 'sanitize_callback' );
 
-					register_setting( $group_slug, $opt_name, [
-							'sanitize_callback' => $sanitize_function,
-							'default'           => $option['default'],
-							'show_in_rest'      => false,
-					] );
+					register_setting( $group_slug, $opt_name, array(
+						'sanitize_callback' => $sanitize_function,
+						'default'           => $option['default'],
+						'show_in_rest'      => false,
+					) );
 					add_settings_field( $opt_name, $option['title'], $render_function, WYSC_PLUGIN_PREFIX . '_settings_page', $section_slug, $opt_name );
 				}
 				add_settings_section( $section_slug, $section['title'], '', WYSC_PLUGIN_PREFIX . '_settings_page' );
@@ -63,7 +63,7 @@ abstract class PageSettingsBase extends PageBase {
 	public function fill_checkbox_field( string $option_name ) {
 		$val   = get_option( $option_name );
 		$val   = $val ? 1 : 0;
-		$check = __( 'Выбрать', 'smart-captcha-yandex' );
+		$check = __( 'Select', 'smart-captcha-yandex' );
 		?>
 		<label for="<?php echo esc_attr( $option_name ); ?>">
 			<input type="checkbox" name="<?php echo esc_attr( $option_name ); ?>"
