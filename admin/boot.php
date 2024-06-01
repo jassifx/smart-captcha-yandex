957dacf6067e7659c0657452da07148c533b2b6e
Jaspreet Singh <156990848+jassifx@users.noreply.github.com>
2024-06-01 16:01:12 +0530

    Update boot.php

--- admin/boot.php
+++ admin/boot.php
@@ -1,13 +1,9 @@
 <?php
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 require_once WYSC_PLUGIN_DIR . '/admin/includes/base/class.page-base.php';
 require_once WYSC_PLUGIN_DIR . '/admin/includes/base/class.page-settings-base.php';
-//require_once WYSC_PLUGIN_DIR . '/admin/pages/class.page-settings.php';
-//require_once WYSC_PLUGIN_DIR . '/admin/pages/class.page-main.php';
 
 $pages_dir = WYSC_PLUGIN_DIR . '/admin/pages/';
 foreach ( scandir( $pages_dir ) as $page ) {
