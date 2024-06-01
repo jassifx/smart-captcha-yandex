4dfc430236cf2f78e834fe49d45937691736e26b
Jaspreet Singh <156990848+jassifx@users.noreply.github.com>
2024-06-01 16:10:44 +0530

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -7,22 +7,21 @@ use WYSC\Plugin;
  *
  * @return bool
  */
-function wysc_check_smart_captcha( $token ) {
+function wysc_check_smart_captcha( string $token ): bool {
 	$server_key = Plugin::getOption( 'server_token' );
 
 	$args     = http_build_query( [
 		'secret' => $server_key,
 		'token'  => sanitize_text_field( $token ),
-		'ip'     => sanitize_text_field( $_SERVER['REMOTE_ADDR'] ),
+		'ip'     => sanitize_text_field( $_SERVER['REMOTE_ADDR'] ?? '' ),
 	] );
-	$response = wp_remote_get( "https://captcha-api.yandex.ru/validate?{$args}" );
+	$response = wp_safe_remote_get( "https://captcha-api.yandex.ru/validate?{$args}" );
 
-	if ( wp_remote_retrieve_response_code( $response ) === 200 ) {
-		$result = json_decode( $response['body'] ?? [], true );
-		if ( 'ok' === $result['status'] ) {
-			return true;
-		}
+	if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) !== 200 ) {
+		return false;
 	}
 
-	return false;
+	$result = json_decode( wp_remote_retrieve_body( $response ), true );
+
+	return ( isset( $result['status'] ) && $result['status'] === 'ok' );
 }
